f8020e8931894d05d48b9eca84d854b3c48d229a
sanandin <anand.s@infosys.com>
2016-07-12 12:30:02 +0530

    Update video.js
    Video URL Updated

--- js/video.js
+++ js/video.js
@@ -1,7 +1,8 @@
 window.onload = function() { 
 	var video = document.getElementById('video');
 	var source = document.createElement('source');
-	source.setAttribute('src', 'http://www.w3schools.com/html/mov_bbb.ogg');
+//	source.setAttribute('src', 'http://www.w3schools.com/html/mov_bbb.ogg');
+	source.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/e/ef/Big_Buck_Bunny_Trailer_1080p.ogg');
 	video.appendChild(source);
 	video.play();
 
